457c010c2748071428400a4a69e6655e241748b2
Iyadydk <iyad_yadek@hotmail.com>
2023-10-20 17:11:25 +0300

    run comands

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://www.amazon.com/',
+    retries: 1,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
